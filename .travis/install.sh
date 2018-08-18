6006639cab9f24cee93a8d0b1d667472836d1969
fjaviersanchez <francs1@uci.edu>
2018-08-18 10:00:05 -0700

    another fix

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,9 +12,9 @@ mv lib $TRAVIS_BUILD_DIR/
 mv include $TRAVIS_BUILD_DIR/
 mv bin $TRAVIS_BUILD_DIR/
 export PATH=$HOME/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRAVIS_BUILD_DIR/lib
-export LDFLAGS="-L$TRAVIS_BUILD_DIR/lib"
-export CPPFLAGS="-I$TRAVIS_BUILD_DIR/include"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRAVIS_BUILD_DIR/lib:/usr/local/lib
+export LDFLAGS="-L$TRAVIS_BUILD_DIR/lib -L/usr/local/lib"
+export CPPFLAGS="-I$TRAVIS_BUILD_DIR/include -I/usr/local/include"
 
 #### Install GSL2.0+ ####
 wget http://mirror.rise.ph/gnu/gsl/gsl-2.4.tar.gz && tar xvzf gsl-2.4.tar.gz && cd gsl-2.4 &&  ./configure && make && sudo make install && cd ..
